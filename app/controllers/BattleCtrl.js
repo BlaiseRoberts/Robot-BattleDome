89ba60ebcf5f91eb9d29e0a8675e179f6e701413
Blaise Roberts <blaiseroberts@Blaises-MacBook-Pro-2.local>
2017-03-10 23:30:25 -0600

    added responsive layout and some fun combat functions and balancing

--- app/controllers/BattleCtrl.js
+++ app/controllers/BattleCtrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-app.controller('BattleCtrl', function ($scope, GameFactory, $route) {
+app.controller('BattleCtrl', function ($scope, GameFactory, $route, BotFactory) {
 
 /////////////////////
 //Initalize Stuff
@@ -12,19 +12,35 @@ app.controller('BattleCtrl', function ($scope, GameFactory, $route) {
 
 	
 ////////////////////////////////////////////////
-//Set up Player and Foe along with their Health
+//Set up Player and Foe
 ////////////////////////////////////////////////
 	$scope.self = GameFactory.getSelf();
 	$scope.foe = GameFactory.getFoe();
 
+	//Make Enemy Class and Name
+	if($scope.foe.gameCount > 1){
+		let randomBots = ["Hacker", "Technomancer", "ProtoSoldier", "CyberAssassin", "BattleDroid", "D18Tank"];
+		let botIndex = Math.floor(Math.random()*6);
+		$scope.foe = Object.create(BotFactory.getBot(randomBots[botIndex]));
+		let randomNames = ["Andrew", "Roxas", "Whitney", "Brenda", "Meg", "Gilbert"];
+		let nameIndex = Math.floor(Math.random()*6);
+		$scope.foe.name = randomNames[nameIndex];
+	}
 
-
-
+	//Add lvls to Player and Foe
+	$scope.self.minHealth += $scope.self.gameCount * 10;
+	$scope.self.maxHealth += $scope.self.gameCount * 10;
+	$scope.self.minDamage += $scope.self.gameCount * 10;
+	$scope.self.maxDamage += $scope.self.gameCount * 10;
+	$scope.foe.minHealth += $scope.self.gameCount * 7;
+	$scope.foe.maxHealth += $scope.self.gameCount * 7;
+	$scope.foe.minDamage += $scope.self.gameCount * 7;
+	$scope.foe.maxDamage += $scope.self.gameCount * 7;
 
 
 
 //////////////////////
-//////Define Health
+//////Define Health for Game
 //////////////////////
 
 	function getRandomInt(min, max) {
@@ -49,8 +65,8 @@ app.controller('BattleCtrl', function ($scope, GameFactory, $route) {
 		$scope.foe.health -= damage;
 		$scope.foe.health = Math.max(0, $scope.foe.health);
 		$scope.foeHealth = ($scope.foe.health / foeTotalHealth)*100;
-		Materialize.toast('-'+damage+' dmg', 1000, 'red darken-2 rounded right-toast');
-		let combatLogText = `<p>Turn ${turnSelf}:  ${$scope.self.name} attacked, dealing ${damage}!</p>`;
+		Materialize.toast('-'+damage+' dmg', 2000, 'blue-grey darken-2 rounded right-toast');
+		let combatLogText = `<p>Turn ${turnSelf}:  ${$scope.self.name} attacked, dealing ${damage} damage!</p>`;
 		$('#combat-log').prepend(combatLogText);
 		turnSelf++;
 		if($scope.foe.health === 0){
@@ -60,13 +76,65 @@ app.controller('BattleCtrl', function ($scope, GameFactory, $route) {
 		window.setTimeout(foeAttacks, 500);
 	};
 
+	let specialAttack = ()=>{
+		if ($scope.self.trait === "Electro-Cloak"){
+			let combatLogText = `<p>Turn ${turnSelf}:  ${$scope.self.name} activates their Electro-Cloak and scrables systems, vanishing.</p>`;
+			$('#combat-log').prepend(combatLogText);
+			turnSelf++;
+			let damage = getRandomInt($scope.self.minDamage, $scope.self.maxDamage);
+			$scope.foe.health -= damage;
+			$scope.foe.health = Math.max(0, $scope.foe.health);
+			$scope.foeHealth = ($scope.foe.health / foeTotalHealth)*100;
+			Materialize.toast('-'+damage+' dmg', 2000, 'blue-grey darken-2 rounded right-toast');
+			combatLogText = `<p>Turn ${turnFoe}:  ${$scope.foe.name} is disoriented and ambushed by ${$scope.self.name} for ${damage} damage!</p>`;
+			$('#combat-log').prepend(combatLogText);
+			$scope.hasArmor = true;
+			$scope.armorTurn = turnSelf;
+			turnFoe++;
+			if($scope.foe.health === 0){
+				$('#youWin').modal('open');
+			}
+		} if ($scope.self.trait === "Nano Recovery"){
+			let healing = (getRandomInt($scope.self.minDamage, $scope.self.maxDamage)+50);
+			$scope.self.health += healing;
+			$scope.selfHealth = ($scope.self.health / selfTotalHealth)*100;
+			Materialize.toast('+'+healing, 2000, 'green darken-2 rounded left-toast');
+			let combatLogText = `<p>Turn ${turnSelf}:  ${$scope.self.name} regenerates it's flesh with Nano Recovery bots for ${healing} health!</p>`;
+			$('#combat-log').prepend(combatLogText);
+			turnSelf++;
+			window.setTimeout(foeAttacks, 500);
+		} if ($scope.self.trait === "Alloy Armor"){
+			$scope.hasArmor = true;
+			$scope.armorTurn = turnSelf;
+			let combatLogText = `<p>Turn ${turnSelf}:  ${$scope.self.name}'s Alloy Armor is strengthened for the next 2 turns!</p>`;
+			$('#combat-log').prepend(combatLogText);
+			turnSelf++;
+			window.setTimeout(foeAttacks, 500);
+		}
+	};
+
+
+	$scope.hasArmor = false;
+
 	let foeAttacks = ()=>{
 		let damage = getRandomInt($scope.foe.minDamage, $scope.foe.maxDamage);
+		if ($scope.armorTurn === turnSelf-4){
+			$scope.hasArmor = false;
+			let armorText = `<p>Turn ${turnFoe}:  ${$scope.self.name}'s armor fades.</p>`;
+			$('#combat-log').prepend(armorText);
+		}
+		if ($scope.hasArmor){
+			if ($scope.self.trait === "Alloy Armor"){
+				damage = Math.floor(damage/2); 	
+			} else {
+				damage = Math.floor(damage/1.2); 	
+			}
+		}
 		$scope.self.health -= damage;
 		$scope.self.health = Math.max(0, $scope.self.health);
 		$scope.selfHealth = ($scope.self.health / selfTotalHealth)*100;
-		Materialize.toast('-'+damage+' dmg', 1000,'rounded red darken-2 left-toast');
-		let combatLogText = `<p>Turn ${turnFoe}:  ${$scope.foe.name} attacked, dealing ${damage}!</p>`;
+		Materialize.toast('-'+damage+' dmg', 2000,'rounded red darken-2 left-toast');
+		let combatLogText = `<p>Turn ${turnFoe}:  ${$scope.foe.name} attacked, dealing ${damage} damage!</p>`;
 		$('#combat-log').prepend(combatLogText);
 		turnFoe++;
 		$scope.$apply();
@@ -81,9 +149,18 @@ app.controller('BattleCtrl', function ($scope, GameFactory, $route) {
 ////////////////////////////////////
 	var turnSelf = 1;
 	var turnFoe = 1; 
+	$scope.haveSpecial = false;
+
 
 	$scope.makeAttack = ()=>{
 		attackFoe();
+		if (turnSelf % 5 === 0){
+			$scope.haveSpecial = true;
+		}
+	};
+	$scope.useSpecial = ()=>{
+		specialAttack();
+		$scope.haveSpecial = false;
 	};
 
 	$scope.continue = ()=>{
--- app/factories/BotFactory.js
+++ app/factories/BotFactory.js
@@ -32,47 +32,53 @@ Android.type = "Android";
 let Bot = {};
 
 Bot.Hacker = Object.create(CyberPunk);
-Bot.Hacker.minHealth = 150;
-Bot.Hacker.maxHealth = 200;
+Bot.Hacker.minHealth = 350;
+Bot.Hacker.maxHealth = 400;
 Bot.Hacker.minDamage = 30;
 Bot.Hacker.maxDamage = 60;
 Bot.Hacker.model = "Hacker";
+Bot.Hacker.image = "images/Female_Hacker.jpg";
 
 Bot.Technomancer = Object.create(CyberPunk);
-Bot.Technomancer.minHealth = 120;
-Bot.Technomancer.maxHealth = 180;
+Bot.Technomancer.minHealth = 320;
+Bot.Technomancer.maxHealth = 480;
 Bot.Technomancer.minDamage = 50;
 Bot.Technomancer.maxDamage = 75;
 Bot.Technomancer.model = "Technomancer";
+Bot.Technomancer.image = "images/Technomancer.png";
 
 
 Bot.ProtoSoldier = Object.create(Cyborg);
-Bot.ProtoSoldier.minHealth = 180;
-Bot.ProtoSoldier.maxHealth = 230;
+Bot.ProtoSoldier.minHealth = 380;
+Bot.ProtoSoldier.maxHealth = 430;
 Bot.ProtoSoldier.minDamage = 40;
 Bot.ProtoSoldier.maxDamage = 50;
-Bot.ProtoSoldier.model = "Proto Soldier";
+Bot.ProtoSoldier.model = "Proto-Soldier";
+Bot.ProtoSoldier.image = "images/Proto_Soldier.png";
 
 Bot.CyberAssassin = Object.create(Cyborg);
-Bot.CyberAssassin.minHealth = 130;
-Bot.CyberAssassin.maxHealth = 200;
+Bot.CyberAssassin.minHealth = 330;
+Bot.CyberAssassin.maxHealth = 400;
 Bot.CyberAssassin.minDamage = 5;
 Bot.CyberAssassin.maxDamage = 100;
 Bot.CyberAssassin.model = "Cyber Assassin";
+Bot.CyberAssassin.image = "images/Cyber_Assassin.jpg";
 
-Bot.PyroDroid = Object.create(Android);
-Bot.PyroDroid.minHealth = 140;
-Bot.PyroDroid.maxHealth = 200;
-Bot.PyroDroid.minDamage = 30;
-Bot.PyroDroid.maxDamage = 85;
-Bot.PyroDroid.model = "Pyro Droid";
+Bot.BattleDroid = Object.create(Android);
+Bot.BattleDroid.minHealth = 340;
+Bot.BattleDroid.maxHealth = 400;
+Bot.BattleDroid.minDamage = 30;
+Bot.BattleDroid.maxDamage = 85;
+Bot.BattleDroid.model = "Battle Droid";
+Bot.BattleDroid.image = "images/Battle_Droid.png";
 
 Bot.D18Tank = Object.create(Android);
-Bot.D18Tank.minHealth = 220;
-Bot.D18Tank.maxHealth = 250;
+Bot.D18Tank.minHealth = 420;
+Bot.D18Tank.maxHealth = 450;
 Bot.D18Tank.minDamage = 0;
 Bot.D18Tank.maxDamage = 70;
 Bot.D18Tank.model = "D18-Tank";
+Bot.D18Tank.image = "images/D18_Tank.jpg";
 
 
 /////////////
